6c228f937595964f2847ffb710cdb8480c941484
Andre Thomas <drethedevjs@gmail.com>
2025-03-07 06:46:45 -0500

    Added Footer

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,24 @@
+<template>
+  <footer>
+    <p>Copyright © {{ new Date().getFullYear() }} Eleventh & Pemberton</p>
+    <p>
+      Powered by
+      <a
+        href="https://csra-web-services.vercel.app/"
+        target="_blank"
+        class="text-secondary font-medium hover:text-primary"
+        >CSRA Web Services</a
+      >
+    </p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  @apply h-24 flex flex-col items-center justify-center mt-5;
+}
+
+p {
+  @apply text-primary text-base ml-5 mb-3;
+}
+</style>
